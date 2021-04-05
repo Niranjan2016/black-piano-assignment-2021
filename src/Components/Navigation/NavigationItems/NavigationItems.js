57a66af55926c31af384b947f40765c6a0b956d0
Niranjan Nimbalkar <niranjan.nimbalkar@gmail.com>
2021-04-05 16:24:37 +0530

    Committing Toolbar and navigation for app

---
+++ src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./NavigationItems.module.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const navigationItems = (props) => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>
+      Movies List
+    </NavigationItem>
+    <NavigationItem link="/search">Search Movie </NavigationItem>
+  </ul>
+);
+
+export default navigationItems;
