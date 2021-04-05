4bfcf38d8afb5e2c37498912851b83a55a006e8b
Niranjan Nimbalkar <niranjan.nimbalkar@gmail.com>
2021-04-05 17:23:12 +0530

    Adding fetch button in movielist container component

--- src/Containers/MovieList/MovieList.js
+++ src/Containers/MovieList/MovieList.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
-// import classes from './MovieList.module.css';
+// import classes from "./MovieList.module.css";
+import Aux from "../../hoc/Aux/Aux";
+import Button from "../../Components/UI/Button/Button";
 
 class MovieList extends Component {
+  state = {
+    movieList: [],
+    isFetchingData: false,
+  };
+
+  fetchMoviesListHandler = () => {
+    console.log("Clicked on fetch Movies");
+  };
+
   render() {
-    return <p>This is a class component</p>;
+    return (
+      <Aux>
+        <p>
+          Here we will be populating the list of movies reviewed in New York
+          Times. To fetch the list of movies reviewed please click "Fetch
+          Movies"
+        </p>
+        <div style={{ textAlign: "center" }}>
+          <Button btnType="Success" clicked={this.fetchMoviesListHandler}>
+            Fetch Movies
+          </Button>
+        </div>
+      </Aux>
+    );
   }
 }
 export default MovieList;
