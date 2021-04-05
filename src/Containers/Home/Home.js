ea50d299b308c794b2944624ad9c8ad8af0297e0
Niranjan Nimbalkar <niranjan.nimbalkar@gmail.com>
2021-04-05 16:25:41 +0530

    Adding main home container

---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+// import classes from "./Home.module.css";
+
+class Home extends Component {
+  render() {
+    return <p>This is a Home component</p>;
+  }
+}
+export default Home;
